e7e707a9ed94e175b76e691cfac78c50cdd3af57
Douglas Jasper <doug.jasper@trios.com>
2026-05-05 08:59:33 -0400

    Prop Spreading

--- src/Checklist/Checklist.tsx
+++ src/Checklist/Checklist.tsx
@@ -1,18 +1,21 @@
+import type { ComponentPropsWithoutRef } from "react";
+
 type Props<Data> = {
     data: Data[];
     id: keyof Data;
     primary: keyof Data;
     secondary: keyof Data;
-};
+} & ComponentPropsWithoutRef<'ul'>;
 
 export function Checklist<Data>({
     data,
     id,
     primary,
-    secondary,    
+    secondary,
+    ...ulProps    
   }: Props<Data>) {
     return (
-        <ul>
+        <ul {...ulProps}>
             {data.map((item) => {
                 const idValue = item[id] as unknown;
                 if (typeof idValue != 'string' && typeof idValue != 'number')
